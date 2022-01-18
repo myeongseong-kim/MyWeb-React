a566b87a37ca30771c6bb5bccc3c7e52ca3e83b6
MyeongseongKim <marqus.kim@gmail.com>
2022-01-18 18:44:30 +0900

    Add Footer

---
+++ my-web/src/components/Footer.js
@@ -0,0 +1,41 @@
+import './Footer.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faYoutube, faLinkedin, faInstagram } from '@fortawesome/free-brands-svg-icons'
+import { faSquareFull } from '@fortawesome/free-solid-svg-icons'
+
+
+function Footer() {
+    let year = new Date().getFullYear();
+
+    return (
+        <div className="Footer">
+
+            {/* socials: github, youtube, linkedin, instagram */}
+            <div className="socials">
+                <div className="icon" onClick={() => window.open("https://github.com/MyeongseongKim", "_blank")}>
+                    <FontAwesomeIcon icon={faGithub} className="logo color github" />
+                    <FontAwesomeIcon icon={faGithub} className="logo bw github" />
+                </div>
+                <div className="icon" onClick={() => window.open("https://www.youtube.com/channel/UCNO9pBEjRyb-8NW2R75SeUQ", "_blank")}>
+                    <FontAwesomeIcon icon={faYoutube} className="logo color youtube" />
+                    <FontAwesomeIcon icon={faYoutube} className="logo bw youtube" />
+                </div>
+                <div className="icon" onClick={() => window.open("https://www.linkedin.com/in/myeongseong-kim-27a30015b", "_blank")}>
+                    <FontAwesomeIcon icon={faLinkedin} className="logo color linkedin" />
+                    <FontAwesomeIcon icon={faLinkedin} className="logo bw linkedin" />
+                </div>
+                <div className="icon" onClick={() => window.open("https://www.instagram.com/", "_blank")}>
+                    <FontAwesomeIcon icon={faInstagram} mask={faSquareFull} inverse className="logo color instagram" />
+                    <FontAwesomeIcon icon={faInstagram} className="logo bw instagram" />
+                </div>
+            </div>
+
+            {/* a copyright */}
+            <div className="copyright">
+                © {year} Myeongseong Kim
+            </div>
+        </div>
+    );
+}
+
+export default Footer;
--- my-web/src/index.js
+++ my-web/src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+import Footer from './components/Footer';
+
 import reportWebVitals from './reportWebVitals';
 
 
 ReactDOM.render(
     <React.StrictMode>
+        <Footer />
     </React.StrictMode>,
     document.getElementById('root')
 );
