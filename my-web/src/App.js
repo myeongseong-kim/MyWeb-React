0418f71fdf9db7cf7aa254ecb4e03a8f800d96b7
MyeongseongKim <marqus.kim@gmail.com>
2022-01-25 18:58:46 +0900

    Create Pages Applied Router

--- my-web/src/App.js
+++ my-web/src/App.js
@@ -1,20 +1,32 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+import './App.css';
 import Header from './components/Header';
 import Nav from './components/Nav';
 import Main from './components/Main';
 import Footer from './components/Footer';
 
+import Home from './pages/Home'
+import About from './pages/About'
+import Projects from './pages/Projects'
+import Contact from './pages/Contact'
+
 
 function App() {
     return (
-        <>
+        <Router>
             <Header />
             <Nav />
-            <Main />
+            {/* <Main /> */}
+            <Routes>
+                <Route exact path='/' element={<Home />} />
+                <Route path='/about' element={<About />} />
+                <Route path='/projects' element={<Projects />} />
+                <Route path='/contact' element={<Contact />} />
+            </Routes>
             <Footer />
-        </>
+        </Router>
     );
 }
 
--- my-web/src/components/Nav.js
+++ my-web/src/components/Nav.js
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom'
+
 import './Nav.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
@@ -6,7 +8,7 @@ import { faBars } from '@fortawesome/free-solid-svg-icons'
 function Nav() {
     return (
         <nav>
-            <div className="nav-logo"> myeongseong.kim </div>
+            <Link to='/' className="nav-logo"> myeongseong.kim </Link>
 
             <button type="button" className="nav-toggle" aria-controls="navbar-menu" aria-expanded="false">
                 <FontAwesomeIcon icon={faBars} />
@@ -14,9 +16,9 @@ function Nav() {
 
             <div className="nav-menu">
                 <ul>
-                    <li> About </li>
-                    <li> Projects </li>
-                    <li> Contact </li>
+                    <li> <Link to='/about' className='menu-item'> About </Link> </li>
+                    <li> <Link to='/projects' className='menu-item'> Projects </Link> </li>
+                    <li> <Link to='/contact' className='menu-item'> Contact </Link> </li>
                 </ul>
             </div>
         </nav>
