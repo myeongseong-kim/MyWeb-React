491a6c642e26409784eb654e6184054c479050a9
MyeongseongKim <marqus.kim@gmail.com>
2022-10-16 01:31:06 +0900

    Map

--- my-web/src/pages/Contact.jsx
+++ my-web/src/pages/Contact.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import styled, { css } from "styled-components";
+
+import Map from '../components/Map';
 
 
 const StyledContact = styled.div`
@@ -12,11 +13,28 @@ const StyledContact = styled.div`
     min-height: 100vh;
 `;
 
+const MapContainer = styled.div`
+    width: 100%;
+    height: 400px;
+`;
+
+
+// Dept.Industrial Design, KAIST
+// Plus code : 99F6+FR Daejeon
+// lat : 36.373688, lng : 127.362063
+const POS_ID_KAIST = {
+    lat: 36.373688,
+    lng: 127.362063
+};
+
 
 const Contact = () => {
     return (
         <StyledContact>
             <h1> Contact </h1>
+            <MapContainer>
+                <Map pos={POS_ID_KAIST} zoom={18} />
+            </MapContainer>
         </StyledContact>
     );
 }
