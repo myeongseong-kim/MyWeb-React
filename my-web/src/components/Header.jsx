abce6f20c1251174880e1a472951fe38e8ac9b0a
MyeongseongKim <marqus.kim@gmail.com>
2022-10-18 22:35:41 +0900

    Minor Header Update

--- my-web/src/components/Header.jsx
+++ my-web/src/components/Header.jsx
@@ -99,11 +99,12 @@ const HeaderPhrase = styled.div`
 
 const TranslucentBox = styled.div`
     position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
     width: 100%;
     height: 100%;
+    top: 0%;
+    bottom: 0%;
+    left: 0%;
+    right: 0%;
 
     background-color: black;
     opacity: 0.625;
